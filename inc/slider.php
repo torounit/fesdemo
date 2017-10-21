479c0cbbc1e347e3e52a8068f76517b00dc94a5d
Toro_Unit (Hiroshi Urabe) <mail@torounit.com>
2017-10-21 15:48:20 +0900

    add comments

--- inc/slider.php
+++ inc/slider.php
@@ -1,4 +1,26 @@
 <?php
+/**
+ * Sample for custom slider.
+ */
+
+/**
+ * Add assets for customizer.
+ */
+function fesdemo_slider_scripts() {
+	wp_enqueue_style( 'fesdemo-swiper', 'https://cdnjs.cloudflare.com/ajax/libs/Swiper/3.4.2/css/swiper.min.css' );
+	wp_enqueue_script( 'fesdemo-swiper', 'https://cdnjs.cloudflare.com/ajax/libs/Swiper/3.4.2/js/swiper.jquery.js', array( 'jquery' ), '3.4.2', true );
+	wp_enqueue_script( 'fesdemo-slider', get_template_directory_uri() . '/js/slider.js', array( 'fesdemo-swiper' ), false, true );
+}
+add_action( 'wp_enqueue_scripts', 'fesdemo_slider_scripts' );
+
+/**
+ * Slide count.
+ *
+ * @return int
+ */
+function fesdemo_slider_count() {
+	return 3;
+}
 
 /**
  * Customizer Setting for Slider.
@@ -11,9 +33,17 @@ function fesdemo_slider_customize_register( WP_Customize_Manager $wp_customize )
 		'title' => __( 'Slides' ),
 	) );
 
-	foreach ( range( 1, 3 ) as $index ) {
+	foreach ( range( 1, fesdemo_slider_count() ) as $index ) {
 
 		$id = 'slide_' . $index;
+
+		/**
+		 * Add setting section.
+		 */
+		$wp_customize->add_section( $id, array(
+			'title' => sprintf( __( 'Slide %s' ), $index ),
+			'panel' => 'slides',
+		) );
 
 		/**
 		 * Set default value for customizer.
@@ -23,20 +53,19 @@ function fesdemo_slider_customize_register( WP_Customize_Manager $wp_customize )
 			'transport' => 'postMessage',
 		) );
 
+		/**
+		 * For Selective refresh.
+		 */
 		$wp_customize->selective_refresh->add_partial( $id, array(
 			'selector'            => '#'.$id,
 			'render_callback'     => 'fesdemo_slider_render',
-			'fallback_refresh'    => false,
 		) );
 
-		$wp_customize->add_section( $id, array(
-			'title' => sprintf( __( 'Slide %s' ), $index ),
-			'panel' => 'slides',
-		) );
-
+		/**
+		 * Control form setting.
+		 */
 		$control = new WP_Customize_Image_Control( $wp_customize, $id, array(
 				'label'          => __( 'Upload an Image' ),
-				'allow_addition' => true,
 				'section'        => $id,
 				'active_callback' => 'is_front_page',
 //				'width'          => 1920,
@@ -53,27 +82,15 @@ add_action( 'customize_register', 'fesdemo_slider_customize_register' );
 /**
  * Slider markup for customizer preview.
  *
- * @param WP_Customize_Partial|null $partial
+ * @param WP_Customize_Partial $partial
  */
-function fesdemo_slider_render( $partial = null ) {
+function fesdemo_slider_render( WP_Customize_Partial $partial ) {
 	$images = get_theme_mod( $partial->id );
 	if ( $images ): ?>
 		<img src="<?php echo esc_url( $images ); ?>" alt="">
 	<?php
 	endif;
 }
-
-/**
- * Add assets for customizer.
- */
-function fesdemo_slider_scripts() {
-
-	wp_enqueue_style( 'fesdemo-swiper', 'https://cdnjs.cloudflare.com/ajax/libs/Swiper/3.4.2/css/swiper.min.css' );
-	wp_enqueue_script( 'fesdemo-swiper', 'https://cdnjs.cloudflare.com/ajax/libs/Swiper/3.4.2/js/swiper.jquery.js', array( 'jquery' ), '3.4.2', true );
-	wp_enqueue_script( 'fesdemo-slider', get_template_directory_uri() . '/js/slider.js', array( 'fesdemo-swiper' ), false, true );
-}
-
-add_action( 'wp_enqueue_scripts', 'fesdemo_slider_scripts' );
 
 /**
  * Binds JS handlers to make Theme Customizer preview reload changes asynchronously.
